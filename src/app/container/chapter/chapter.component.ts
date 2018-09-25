452d7e875ef3b9565dbefcef10d7c7d2b3adac51
qiuz <zhu_anly@163.com>
2018-09-25 18:25:59 +0800

    page read

--- src/app/container/chapter/chapter.component.ts
+++ src/app/container/chapter/chapter.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, ElementRef, OnDestroy } from '@angular/core';
+import { Component, OnInit, ElementRef, OnDestroy, AfterViewChecked } from '@angular/core';
 import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 import { HttpService } from '../../core/http/http.service';
 import { LocalStorage } from '../../common/local-storage';
-import { tap } from '../../common/tap';
+import { tap, move } from '../../common/tap';
 import {Location} from '@angular/common';
 
 @Component({
@@ -16,6 +16,8 @@ export class ChapterComponent implements OnInit, OnDestroy {
   fontSize = LocalStorage.getItem('fontSize') || 16;
   pageSetting = false;
   day = false;
+  page = 0;
+  pageSize = 0;
 
   constructor(
     private route: ActivatedRoute,
@@ -30,7 +32,6 @@ export class ChapterComponent implements OnInit, OnDestroy {
     chapterId = this.route.snapshot.params['chapterId'];
     this.getChapter(bookId, chapterId);
     document.body.style.backgroundColor = '#c4b395';
-
     tap(document, event => {
       const x = (<any>event).changedTouches[0].pageX;
       const y = (<any>event).changedTouches[0].pageY - window.scrollY;
@@ -39,6 +40,14 @@ export class ChapterComponent implements OnInit, OnDestroy {
         this.pageConfig = !this.pageConfig;
         document.body.style.overflow = this.pageConfig ? 'hidden' : '';
       }
+    });
+
+    tap(this.el.nativeElement.querySelector('.content'), event => {
+      this.next();
+    });
+
+    move(this.el.nativeElement.querySelector('.content'), event => {
+      this.next('prev');
     });
   }
 
@@ -54,9 +63,11 @@ export class ChapterComponent implements OnInit, OnDestroy {
     this.httpService.get('getChapter', {bookId, chapterId})
       .subscribe(res => {
         this.chapter = res;
+        this.page = 0;
         if (!(<any>this.chapter).content) {
           (<any>this.chapter).content = `\n\t\t\t<div>当前章节暂无内容</div>`;
         }
+        this.pageTransform();
         window.scrollTo(0, 0);
         this.location.replaceState(`/book/${bookId}/${chapterId}`);
       });
@@ -85,6 +96,23 @@ export class ChapterComponent implements OnInit, OnDestroy {
     this.el.nativeElement.querySelector('.page-header').style.backgroundColor = this.day ? '#1a1a1a' : '#c4b395';
     document.body.style.color = this.day ? 'rgba(255,255,255,.5)' : '#33373d';
     this.el.nativeElement.querySelector('.page-header').style.color = this.day ? 'rgba(255,255,255,.5)' : 'rgba(0,0,0,.4)';
+  }
+
+  next(type?: string) {
+    if (this.pageSize === 0) {
+      this.pageSize = Math.floor(document.body.querySelector('.content').scrollWidth / (window.innerWidth - 16));
+    }
+    if (this.page === this.pageSize) {
+      this.getChapter((<any>this.chapter).bookId, (<any>this.chapter).next);
+      return;
+    }
+    type === 'prev' ? this.page-- : this.page++;
+    this.pageTransform();
+  }
+
+  pageTransform() {
+    const x = (window.innerWidth - 16) * this.page;
+    this.el.nativeElement.querySelector('.content').style.transform = `translateX(-${x}px)`;
   }
 
   ngOnDestroy() {
