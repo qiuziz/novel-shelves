afaf211bf2fcdbf770132485daa8813d5c41d9fd
qiuz <zhu_anly@163.com>
2018-10-15 17:17:57 +0800

    redirectTo shelves

--- src/app/container/container-routing.module.ts
+++ src/app/container/container-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from '../components/layout/layout.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'search', pathMatch: 'full' },
+  { path: '', redirectTo: 'shelves', pathMatch: 'full' },
   { path: '',
     component: LayoutComponent,
     children: [
