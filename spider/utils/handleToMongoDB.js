49184dc3ce9b29246d72537d06cd0f1f3c5f8f4c
qiuz <zhu_anly@163.com>
2018-10-17 14:17:20 +0800

    mongodb find > 100 data error

--- spider/utils/handleToMongoDB.js
+++ spider/utils/handleToMongoDB.js
@@ -3,13 +3,13 @@
  * @Github: <https://github.com/qiuziz>
  * @Date: 2018-09-07 19:17:57
  * @Last Modified by: qiuz
- * @Last Modified time: 2018-10-12 18:05:05
+ * @Last Modified time: 2018-10-17 14:16:32
  */
 
 const connect = require('../db.js');
 
 const handleToMongoDB = {
-  insert: (collectionName, data) => connect((err, db) => {
+  insert: (collectionName, data) => connect((err, db, client) => {
   		//连接到表
       const collection = db.collection(collectionName);
   		//插入数据库
@@ -19,9 +19,10 @@ const handleToMongoDB = {
             console.log('Error:'+ err);
             return;
         }
+        client.close();
       });
     }),
-  update: (collectionName, query, data) => connect((err, db) => {
+  update: (collectionName, query, data) => connect((err, db, client) => {
     //连接到表
     const collection = db.collection(collectionName);
     //插入数据库
@@ -34,10 +35,11 @@ const handleToMongoDB = {
           console.log('Error:'+ err);
           return;
       }
+      client.close();
     });
   }),
   findOne: (collectionName, query) => new Promise((resolve, reject) => {
-    connect((err, db) => {
+    connect((err, db, client) => {
       const collection = db.collection(collectionName);
       return collection.findOne(query,  {fields: {_id: 0, url: 0, lastModified: 0}}, function(err, result) {
         if(err)
@@ -46,11 +48,12 @@ const handleToMongoDB = {
             reject(err);
         }
         resolve(result);
+        client.close();
       });
     })
   }),
   find: (collectionName, query) => new Promise((resolve, reject) => {
-    connect((err, db) => {
+    connect((err, db, client) => {
       const collection = db.collection(collectionName);
       return collection.find(query,  {fields: {_id: 0, url: 0, lastModified: 0}}).toArray(function(err, result) {
         if(err)
@@ -59,11 +62,12 @@ const handleToMongoDB = {
             reject(err);
         }
         resolve(result);
+        client.close();
       });
     })
   }),
   findOneNode: (collectionName, query) => new Promise((resolve, reject) => {
-    connect((err, db) => {
+    connect((err, db, client) => {
       const collection = db.collection(collectionName);
       return collection.findOne(query, {fields: {lastModified: 0}}, function(err, result) {
         if(err)
@@ -72,6 +76,7 @@ const handleToMongoDB = {
             reject(err);
         }
         resolve(result);
+        client.close();
       });
     })
   }),
